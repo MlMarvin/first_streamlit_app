32c28f82dec6b7a1dd90275c1356e5bd1778c861
Marvin MARIE-LOUISE <marvin.mlouise@gmail.com>
2023-12-06 11:07:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit
 
-streamlit.title('My Mom'''s new healthy diner')
+streamlit.title('My Mom\'s new healthy diner')
 
 streamlit.header('Breakfast favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
